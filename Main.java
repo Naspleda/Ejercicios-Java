5735bfbf0f877eee498a547243c6bd179ee4d6a6
Naspleda <98424182+Naspleda@users.noreply.github.com>
2022-08-03 12:43:08 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ejercicio e = new Ejercicio();
+        //declaración
+        Ejercicio2 e = new Ejercicio2();
+
+        //instancia
+        e.resolver();
+        e.mostrar();
+    }
