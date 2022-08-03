ebbc9b2c68f4704a37b1b0c89a944a7b7567db4c
Naspleda <98424182+Naspleda@users.noreply.github.com>
2022-08-03 09:29:27 -0300

    Agregando Ejercicios

--- Main.java
+++ Main.java
@@ -1,11 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        //declaración
-        Ejercicio2 e = new Ejercicio2();
+        System.out.println("Hola Mundo desde Java");
 
-        //instancia
-        e.resolver();
-        e.mostrar();
+        //Definimos una variable
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+
+        //var - Inferencia de tipos en Java
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+
+        var miVariableCadena2 = "Nueva Cadena";
+        System.out.println(miVariableCadena2);
     }
 }
